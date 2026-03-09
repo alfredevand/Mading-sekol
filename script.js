b87263d3f4e11637c1aefb0260b4f23947b7c4d5
alfredevand <alfredevanderwirawan@gmail.com>
2026-03-09 17:25:53 +0700

    madingbaru

---
+++ script.js
@@ -0,0 +1,48 @@
+async function loadData(){
+
+let res = await fetch("data.json")
+let data = await res.json()
+
+document.getElementById("pengumuman").innerText = data.pengumuman
+document.getElementById("infoMinggu").innerText = data.infoMinggu
+document.getElementById("videoFrame").src = data.video
+
+updateJadwal(data.jadwal)
+
+}
+
+function updateJadwal(jadwal){
+
+let hari = new Date().toLocaleDateString("id-ID",{weekday:'long'})
+let jam = new Date().getHours()
+
+let pelajaran = "Tidak ada pelajaran"
+
+if(jadwal[hari]){
+
+jadwal[hari].forEach(j => {
+
+if(jam >= j.mulai && jam < j.selesai){
+pelajaran = j.mapel
+}
+
+})
+
+}
+
+document.getElementById("jadwalSekarang").innerText = pelajaran
+
+}
+
+function updateClock(){
+
+let now = new Date()
+let jam = now.toLocaleTimeString("id-ID")
+document.getElementById("clock").innerText = jam
+
+}
+
+updateClock()
+setInterval(updateClock,1000)
+
+loadData()
